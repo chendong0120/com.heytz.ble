6c94bcff0c6823bb1b508be3c07a3bfaf2135d7b
陈东东 <kevinchen@infapp.com>
2016-01-15 22:03:36 +0800

    add startScan stopScan

--- src/android/heytzBle/heytzBle.java
+++ src/android/heytzBle/heytzBle.java
@@ -1,24 +1,74 @@
 package com.heytz.ble;
 
-import org.apache.cordova.CordovaPlugin;
-import org.apache.cordova.CallbackContext;
-
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import com.heytz.ble.sdk.BleService;
+import com.heytz.ble.sdk.IBle;
+import org.apache.cordova.*;
 import org.json.JSONArray;
 import org.json.JSONException;
-import org.json.JSONObject;
+
 
 /**
  * This class echoes a string called from JavaScript.
  */
 public class heytzBle extends CordovaPlugin {
 
+    private IBle mBle;
+    private String TAG = "heytzBle";
+    private Context context;
+    private CallbackContext _callbackcontext;
+    private final BroadcastReceiver mBleReceiver = new BroadcastReceiver() {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (BleService.BLE_NOT_SUPPORTED.equals(action)) {
+                LOG.w(TAG, "Ble not support");
+                //todo 不支持ble
+            } else if (BleService.BLE_DEVICE_FOUND.equals(action)) {
+                // device found
+                Bundle extras = intent.getExtras();
+                final BluetoothDevice device = extras
+                        .getParcelable(BleService.EXTRA_DEVICE);
+                //todo 获取到新的device;
+                LOG.w(TAG, device.getName());
+
+            } else if (BleService.BLE_NO_BT_ADAPTER.equals(action)) {
+                LOG.w(TAG, "No bluetooth adapter");
+                //todo 没有蓝牙设备
+            }
+        }
+    };
+
+    @Override
+    public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+        super.initialize(cordova, webView);
+        // your init code here
+        context = cordova.getActivity().getApplicationContext();
+    }
+
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        _callbackcontext = callbackContext;
+        context.registerReceiver(mBleReceiver, BleService.getIntentFilter());
         if (action.equals("coolMethod")) {
             String message = args.getString(0);
             this.coolMethod(message, callbackContext);
             return true;
         }
+        if (action.equals("startScan")) {
+            this.startScan();
+            return true;
+        }
+        if (action.equals("stopScan")) {
+            this.stopScan();
+            return true;
+        }
+
         return false;
     }
 
@@ -29,4 +79,13 @@ public class heytzBle extends CordovaPlugin {
             callbackContext.error("Expected one non-empty string argument.");
         }
     }
+
+    private void startScan() {
+        mBle.startScan();
+    }
+
+    private void stopScan() {
+        mBle.stopScan();
+    }
+
 }
--- www/heytzBle.js
+++ www/heytzBle.js
@@ -1,5 +1,13 @@
 var exec = require('cordova/exec');
 
-exports.coolMethod = function(arg0, success, error) {
-    exec(success, error, "heytzBle", "coolMethod", [arg0]);
+exports = {
+    coolMethod: function (arg0, success, error) {
+        exec(success, error, "heytzBle", "coolMethod", [arg0]);
+    },
+    startScan: function (success, error) {
+        exec(success, error, "heytzBle", "startScan", []);
+    },
+    stopScan: function (success, error) {
+        exec(success, error, "heytzBle", "stopScan", []);
+    }
 };

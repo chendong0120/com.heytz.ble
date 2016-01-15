295d49ee8f2e8dfd94d10c82fccd2e340d887e20
陈东东 <kevinchen@infapp.com>
2016-01-15 22:43:22 +0800

    修改clobbers

--- www/heytzBle.js
+++ www/heytzBle.js
@@ -1,6 +1,6 @@
 var exec = require('cordova/exec');
 
-exports = {
+module.exports = {
     coolMethod: function (arg0, success, error) {
         exec(success, error, "heytzBle", "coolMethod", [arg0]);
     },

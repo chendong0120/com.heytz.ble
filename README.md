093452650337958f59ba306694ace1b017136df1
陈东东 <kevinchen@infapp.com>
2016-01-17 18:11:42 +0800

    add scan 方法

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # com.heytz.ble
 黑子信息科技ble控制
+
+android
+    修改application  android:name 为:com.heytz.ble.BleApplication
+    例子:
+         android:name="com.heytz.ble.BleApplication"
--- www/heytzBle.js
+++ www/heytzBle.js
@@ -1,9 +1,6 @@
 var exec = require('cordova/exec');
 
 module.exports = {
-    coolMethod: function (arg0, success, error) {
-        exec(success, error, "heytzBle", "coolMethod", [arg0]);
-    },
     startScan: function (success, error) {
         exec(success, error, "heytzBle", "startScan", []);
     },

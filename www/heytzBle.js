9170e1fb68ffc960170f974ef0be9dfb4f6355a2
陈东东 <kevinchen@infapp.com>
2016-01-18 14:53:51 +0800

    添加接口

--- www/heytzBle.js
+++ www/heytzBle.js
@@ -6,5 +6,14 @@ module.exports = {
     },
     stopScan: function (success, error) {
         exec(success, error, "HeytzBle", "stopScan", []);
-    }
+    },
+    isEnabled: function (success, error) {
+        exec(success, error, "HeytzBle", "isEnabled", []);
+    },
+    connect: function (device_id, success, error) {
+        exec(success, error, 'HeytzBle', 'connect', [device_id]);
+    },
+    startNotification: function (device_id, serverUUID, characteristicUUID, success, error) {
+        exec(success, error, 'HeytzBle', 'startNotification', [device_id, serverUUID, characteristicUUID]);
+    },
 };
